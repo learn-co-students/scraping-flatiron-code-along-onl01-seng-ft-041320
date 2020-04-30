0d322238570672c3345eb57a0327db004b04f962
github username <github email address>
2020-04-30 19:52:47 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,31 @@
 require 'nokogiri'
 require 'open-uri'
+require "pry"
 
 require_relative './course.rb'
 
 class Scraper
+
+  attr_reader :doc
+
+  def get_page
+  html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+  @doc = Nokogiri::HTML(html)
+  end
+
+  def get_courses
+    @courses = self.get_page.css("h2")
+  end
+
+  def make_courses
+    self.get_courses.map do |element|
+      new_course = Course.new
+      new_course.title = element.css("h2").text
+      new_course.schedule = element.css(".date").text
+      new_course.description = element.css("p").text
+    end
+  end
+
   
   def print_courses
     self.make_courses
